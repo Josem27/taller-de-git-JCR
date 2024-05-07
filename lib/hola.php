474c29ea26c4061c64f6d4b1a5a560ff5459a82f
Josem27 <josem.cruzregalado.com>
2024-05-07 17:37:07 +0200

    hola usa la clase HolaMundo

--- lib/hola.php
+++ lib/hola.php
@@ -1,6 +1,9 @@
 <?php
 //Autor: Jose Maria Cruz Regalado <josem.cruzregalado@gmail.com>
 // El nombre por defecto es Mundo
+require('HolaMundo.php');
+
 print "Introduce tu nombre:";
 $nombre = trim(fgets(STDIN));
 @print "Hola, {$nombre}\n";
+
