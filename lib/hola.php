04f7ca199cb203834359cd5d23cf2f90ed71dd45
Josem27 <josem.cruzregalado.com>
2024-05-07 17:41:47 +0200

    Programa interactivo

--- lib/hola.php
+++ lib/hola.php
@@ -1,5 +1,6 @@
 <?php
 //Autor: Jose Maria Cruz Regalado <josem.cruzregalado@gmail.com>
 // El nombre por defecto es Mundo
-$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
-@print "Hola, {$nombre}\n";echo "Hola Mundo\n";
+print "Introduce tu nombre:";
+$nombre = trim(fgets(STDIN));
+@print "Hola, {$nombre}\n";
