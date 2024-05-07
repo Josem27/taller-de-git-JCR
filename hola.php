eba11570c5f768f66f9922d8cc521cef34229527
Josem27 <josem.cruzregalado.com>
2024-05-07 17:30:35 +0200

    Añadido el autor del programa y su email

--- hola.php
+++ hola.php
@@ -1,3 +1,5 @@
 <?php
-//Jose Maria Cruz Regalado
-echo "Hola Mundo\n";
+//Autor: Jose Maria Cruz Regalado <josem.cruzregalado@gmail.com>
+// El nombre por defecto es Mundo
+$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+@print "Hola, {$nombre}\n";echo "Hola Mundo\n";
